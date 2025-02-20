7b2b671626c3e5f170eaf3fbdf8d9a1602b81c9d
Ahad-Miah <ahadahmedcc@gmail.com>
2025-02-20 14:16:49 +0600

    added in progress card and done card static

---
+++ src/Components/Done/Done.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Modal from '../MOdal/Modal';
+
+const Done = ({ inputOpen, handleOpen, handleClose, handleAdd, task }) => {
+    return (
+        <div>
+        <div className='bg-white border border-white rounded-lg h-96 p-6 overflow-y-auto' >
+       <h1 className='font-bold mb-2 text-xl'>To-Do</h1>
+       <div className='flex flex-col gap-4'>
+           {
+               task?.map((t) => <div>
+                   <div onClick={() => document.getElementById('my_modal_5').showModal()} className='w-full border border-gray-200 rounded-md py-4 px-2 '>
+                       <h1>{t.title}</h1>
+                   </div>
+               </div>)
+           }
+       </div>
+       {
+           inputOpen && <>
+               <form onSubmit={(e) => handleAdd(e, 'inProgress')} className='border rounded-lg p-2 flex flex-col mt-4 gap-3'>
+                   <input type="text" required name='title' placeholder="Enter A Title" className="input mt-4 input-bordered w-full " />
+                   <textarea required className="textarea w-full textarea-bordered" name='description' placeholder="Enter Description"></textarea>
+                   <div className='flex justify-between'>
+                       <button className='bg-blue-700 w-1/3  mt-5 p-3 rounded-lg font font-semibold text-white'>Add Task</button>
+                       <button onClick={handleClose} className='bg-blue-700 w-1/3  mt-5 p-3 rounded-lg font font-semibold text-white'>X</button>
+                   </div>
+               </form>
+           </>
+       }
+       <div>
+           <button onClick={handleOpen} className={`bg-blue-700 w-full mt-5 p-3 rounded-lg font font-semibold text-white ${inputOpen ? "hidden" : ""}`}>Add a Card +</button>
+       </div>
+       <Modal></Modal>
+   </div>
+   </div>
+    );
+};
+
+export default Done;
--- src/Components/TaskManager/TaskManager.jsx
+++ src/Components/TaskManager/TaskManager.jsx
@@ -1,59 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import Modal from '../MOdal/Modal';
+import ToDo from '../ToDo/ToDo';
+import InProgres from '../InProgress/InProgres';
+import Done from '../Done/Done';
 
 const TaskManager = () => {
-    const [inputOpen,setInputOpen]=useState(false);
-    const [inputValue,setInputValue]=useState("");
-    const [task,setTask]=useState([]);
+    const [inputOpen, setInputOpen] = useState(false);
+    const [task, setTask] = useState([]);
 
-    const handleOpen=()=>{
-       setInputOpen(true);
-
+    const handleOpen = () => {
+        setInputOpen(true);
     }
-    const handleClose=()=>{
+
+    const handleClose = () => {
         setInputOpen(false);
     }
-    const handleAdd=()=>{
-        if (!inputValue.trim()) return;
-        setTask(prevTasks => [...prevTasks, inputValue]);
+    const handleAdd = (e, category) => {
+        e.preventDefault(); 
+        const title = e.target.title.value;
+        const description = e.target.description.value;
+    
+        if (!title.trim()) return;
+        const newTask = { title, description, category };
+        setTask(prevTasks => [...prevTasks, newTask]);
+    
+        e.target.reset();
+    
         setInputOpen(false);
-    }
-    useEffect(() => {
-        console.log(task);
-    }, [task]);
-
+    };
+    
+    // useEffect(() => {
+    //     console.log(task);
+    // }, [task]);
+    console.log(task);
     return (
-        <div className='min-h-screen bg-gray-100 p-5 grid grid-cols-1 md:grid-cols-3 justify-center gap-5 px-10'>
-            <div className='bg-white border border-white rounded-lg h-96 p-6' >
-                <h1 className='font-bold mb-2 text-xl'>To-Do</h1>
-                <div className='flex flex-col gap-4'>
-                    {
-                        task?.map((t)=> <div>
-                            <div className='w-full border border-gray-200 rounded-md py-4 px-2 '>
-                                <h1>{t}</h1>
-                            </div>
-                        </div>)
-                    }
-                </div>
-               {
-                inputOpen &&  <input onChange={(e)=>setInputValue(e.target.value)} type="text" placeholder="Enter A Title" className="input input-bordered w-full " />
-               }
-                <div> 
-                <button onClick={handleOpen} className={`bg-blue-700 w-full mt-5 p-3 rounded-lg font font-semibold text-white ${inputOpen?"hidden":""}`}>Add a Card +</button>
-                {
-                    inputOpen && <div className='flex justify-between'>
-                    <button onClick={handleAdd} className='bg-blue-700 w-1/3  mt-5 p-3 rounded-lg font font-semibold text-white'>Add Task</button>
-                    <button onClick={handleClose} className='bg-blue-700 w-1/3  mt-5 p-3 rounded-lg font font-semibold text-white'>X</button>
-                    </div> 
-                }
-                </div>
-              
-            </div>
-            <div className='bg-white border border-white rounded-lg h-96 p-6' >
-                <h1 className='font-bold text-xl'>In Progress</h1>
-            </div>
-            <div className='bg-white border border-white rounded-lg h-96 p-6' >
-                <h1 className='font-bold text-xl'>Done</h1>
-            </div>
+        <div className='min-h-screen bg-gray-100 py-14 p-5 grid grid-cols-1 md:grid-cols-3 justify-center gap-5 px-10'>
+            <ToDo handleAdd={handleAdd} handleClose={handleClose}handleOpen={handleOpen} inputOpen={inputOpen} task={task}></ToDo>
+            <InProgres handleAdd={handleAdd} handleClose={handleClose}handleOpen={handleOpen} inputOpen={inputOpen} task={task} ></InProgres>
+            <Done handleAdd={handleAdd} handleClose={handleClose}handleOpen={handleOpen} inputOpen={inputOpen} task={task} ></Done>
         </div>
     );
 };
