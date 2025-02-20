ac7801ce3b88d1b40c5cf8f4045df23688709fe0
Ahad-Miah <ahadahmedcc@gmail.com>
2025-02-20 11:21:53 +0600

    implement google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
 import Login from './Components/Login/Login'
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import { auth } from './firebase.init';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
+const provider = new GoogleAuthProvider();
 function App() {
-  
+  const [user,setUser]=useState(null);
+  const [loading,setLoading]=useState(true);
+
+  const googleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then(res => {
+        console.log(res.user);
+      })
+  }
+
+  useEffect(() => {
+
+    const unsubscribe = onAuthStateChanged(auth, CurrentUser => {
+      setUser(CurrentUser);
+      setLoading(false);
+    })
+
+    return () => {
+      unsubscribe();
+    }
+
+  }, [])
+  console.log(user);
 
   return (
-   <div className='min-h-screen'>
-    <div>
-      <h1 className='font-bold text-center p-4 text-4xl bg-gradient-to-r from-purple-900 to-pink-300 text-transparent bg-clip-text '>TaskSprint</h1>
-    <p className='text-center bg-gradient-to-r  from-purple-500 to-pink-500 text-transparent bg-clip-text'>Boost Productivity with smart task handling</p>
+    <div className='min-h-screen'>
+      <div>
+        <h1 className='font-bold text-center p-4 text-4xl bg-gradient-to-r from-purple-900 to-pink-300 text-transparent bg-clip-text '>TaskSprint</h1>
+        <p className='text-center bg-gradient-to-r  from-purple-500 to-pink-500 text-transparent bg-clip-text'>Boost Productivity with smart task handling</p>
+      </div>
+      <div>
+        <Login googleLogin={googleLogin}></Login>
+      </div>
     </div>
-    <div>
-      <Login></Login>
-    </div>
-   </div>
   )
 }
 
